279723b966bed7429711829f5a1570026e11a824
Hartley Brody <hartley.brody@gmail.com>
2013-06-06 13:02:10 -0400

    added basic User model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 
 
 app = Flask(__name__)
@@ -15,6 +16,26 @@ def homepage():
         pass
     else:
         return render_template("homepage.html")
+
+
+class User(db.Model):
+
+    __tablename__ = "rooster_users"
+
+    id =            db.Column(db.Integer, primary_key=True)
+    phone =         db.Column(db.String(30), unique=True)
+    zipcode =       db.Column(db.String(12))
+    alarm_time =    db.Column(db.String(12))
+    time_zone =     db.Column(db.String(12))
+
+    def __init__(self, phone, zipcode, alarm_time, time_zone):
+        self.phone = phone
+        self.zipcode = zipcode
+        self.alarm_time = alarm_time
+        self.time_zone = time_zone
+
+    def __repr__(self):
+        return '<Phone Num %r>' % self.phone
 
 if __name__ == "__main__":
     app.debug = True
