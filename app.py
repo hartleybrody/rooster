baa00703cc9b1c8207f5e365c10aeba3b72defd6
Hartley Brody <hartley.brody@gmail.com>
2013-06-06 18:11:56 -0400

    use alarm_meridian and move view code into needs_message_now() method

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def homepage():
             "zipcode": request.form.get("zipcode"),
             "alarm_hour": request.form.get("alarm-hour"),
             "alarm_minute": request.form.get("alarm-minute"),
+            "alarm_meridian": request.form.get("alarm-meridian"),
             "time_zone": request.form.get("time-zone"),
         }
 
@@ -33,7 +34,7 @@ def homepage():
 
         try:
             db.session.commit()
-            flash("Cock-a-doodle-doo! You'll get your first text at %s:%s" % (data["alarm_hour"], data["alarm_minute"]), "success")
+            flash("Cock-a-doodle-doo! You'll get your first text at %s:%s %s tomorrow..." % (data["alarm_hour"], data["alarm_minute"], data["alarm_meridian"]), "success")
         except IntegrityError:
             try:
                 User.query.filter_by(phone=u.phone)
@@ -54,10 +55,9 @@ def send_all():
     if "localhost" in request.url and app.debug:
         users = User.query.all()
         for user in users:
-            sent = user.send_message()
-            if sent:
-                print "sent msg to %r" % user
-        return "okay", 200
+            print user.send_message()
+
+        return "okay", 201
 
     else:
         return "nope, sorry", 403
@@ -66,40 +66,12 @@ def send_all():
 @app.route("/send/")
 def send_texts():
 
-    # get the current number of seconds since the beginning of the day in UTC
-    utc_now = datetime.utcnow()
-    midnight_utc = datetime.combine(utc_now.date(), time(0))
-    delta = utc_now - midnight_utc
-
-    current_utc_offset_seconds = int(delta.total_seconds())
-    current_utc_offset_hours = current_utc_offset_seconds / (60 * 60)
-    current_utc_offset_minutes = (current_utc_offset_seconds - (60 * 60 * current_utc_offset_hours)) / 60
-
-    return_str = "current UTC time is {h}:{m}\n".format(h=current_utc_offset_hours, m=current_utc_offset_minutes)
-
     users = User.query.all()
     for user in users:
-        alarm_hour = int(user.alarm_hour)
-        time_zone = user.time_zone
+        if user.needs_message_now():
+            print user.send_message()
 
-        if time_zone.startswith("-"):
-            desired_alarm_hour = alarm_hour + int(time_zone.split("-")[1])
-        else:
-            desired_alarm_hour = alarm_hour - int(time_zone)
-
-        if desired_alarm_hour != current_utc_offset_hours:
-            continue
-
-        # allow a bit of variance in the minutes
-        minutes_range = range(current_utc_offset_minutes - 3, current_utc_offset_minutes + 3)
-        minutes_range = map(wrap_minutes, minutes_range)
-
-        if user.alarm_minute not in minutes_range:
-            continue
-
-        user.send_message()
-
-    return return_str
+    return "okay", 201
 
 
 def wrap_minutes(m):
@@ -118,22 +90,69 @@ class User(db.Model):
 
     __tablename__ = "rooster_users"
 
-    id =            db.Column(db.Integer, primary_key=True)
-    phone =         db.Column(db.String(30), unique=True)
-    zipcode =       db.Column(db.String(8))
-    alarm_hour =    db.Column(db.String(2))
-    alarm_minute =  db.Column(db.String(2))
-    time_zone =     db.Column(db.String(3))
+    id =                db.Column(db.Integer, primary_key=True)
+    phone =             db.Column(db.String(30), unique=True)
+    zipcode =           db.Column(db.String(8))
+    alarm_hour =        db.Column(db.String(2))
+    alarm_minute =      db.Column(db.String(2))
+    alarm_meridian =    db.Column(db.String(2))
+    time_zone =         db.Column(db.String(3))
 
-    def __init__(self, phone, zipcode, alarm_hour, alarm_minute, time_zone):
+    def __init__(self, phone, zipcode, alarm_hour, alarm_minute, alarm_meridian, time_zone):
         self.phone = phone
         self.zipcode = zipcode
         self.alarm_hour = alarm_hour
         self.alarm_minute = alarm_minute
+        self.alarm_meridian = alarm_meridian
         self.time_zone = time_zone
 
     def __repr__(self):
         return '<User %r>' % self.phone
+
+    def needs_message_now(self):
+        """
+        Should we text the user right now?
+        """
+
+        # get the current number of seconds since the beginning of the day in UTC
+        utc_now = datetime.utcnow()
+        midnight_utc = datetime.combine(utc_now.date(), time(0))
+        delta = utc_now - midnight_utc
+
+        current_utc_offset_seconds = int(delta.total_seconds())
+        current_utc_offset_hours = current_utc_offset_seconds / (60 * 60)
+        current_utc_offset_minutes = (current_utc_offset_seconds - (60 * 60 * current_utc_offset_hours)) / 60
+
+        # figure out the hour they wanted to wake up
+        alarm_hour = int(self.alarm_hour)
+        if self.alarm_meridian.lower() == "pm":
+            alarm_hour += 12
+
+        # shift the hour for their timezone
+        time_zone = self.time_zone
+        if time_zone.startswith("-"):
+            desired_alarm_hour = alarm_hour + int(time_zone.split("-")[1])
+        else:
+            desired_alarm_hour = alarm_hour - int(time_zone)
+
+        # make sure we don't have any impossible hours
+        if desired_alarm_hour > 23:
+            desired_alarm_hour -= 24
+        elif desired_alarm_hour < 0:
+            desired_alarm_hour += 24
+
+        if desired_alarm_hour != current_utc_offset_hours:
+            return False
+
+        # allow a bit of variance in the minutes
+        minutes_range = range(current_utc_offset_minutes - 3, current_utc_offset_minutes + 3)
+        minutes_range = map(wrap_minutes, minutes_range)
+
+        if self.alarm_minute not in minutes_range:
+            return False
+
+        return True
+
 
     def send_message(self):
         """
