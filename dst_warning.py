98e94c08c3d8d736063c068183185791ed5fc6c6
Hartley Brody <hartley.brody@gmail.com>
2015-11-01 16:34:37 -0500

    update daily savings time warning for the end of summer

--- dst_warning.py
+++ dst_warning.py
@@ -5,15 +5,15 @@ from app import User, sentry, app
 
 def send_texts():
 
-    users = User.query.all(is_active=True)
+    users = User.query.filter_by(is_active=True)
     # users = User.query.filter_by(phone=os.environ["TEST_PHONE_NUM"])
     for user in users:
         if user.is_active:
 
             current_offset = user.time_zone
-            suggested_offset = int(current_offset) + 1
+            suggested_offset = int(current_offset) - 1
 
-            message = "Don't let daylight savings time mess up your alarm!\n\nYour current timezone offset is %s\n\nIf DST has started for you, reply with \"TZ: %s\" to update." % (current_offset, suggested_offset)
+            message = "Don't let daylight savings time mess up your alarm!\n\nYour current timezone offset is %s\n\nIf DST has ended for you, reply with \"TZ: %s\" to update." % (current_offset, suggested_offset)
             try:
                 sent = user.send_message(message, "dst_warning")
                 if sent:
